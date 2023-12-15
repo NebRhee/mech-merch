5fd01f6121a750cb795b40eff11483c9095cb5ce
Alex <alexwhduong@gmail.com>
2023-12-15 18:32:12 -0500

    Setup Client with TW and React Router V6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import HomePage from "./HomePage";
 
 function App() {
+  const router = createBrowserRouter([
+    { path: "/", element: <div>Hello World</div> },
+    {
+      path: "/home",
+      element: <HomePage></HomePage>,
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
